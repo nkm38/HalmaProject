148290a75504d7cbf8f72474d78894150e4cc71b
Jorden Kreps <jakereps@users.noreply.github.com>
2017-05-09 16:17:47 -0700

    ENH: Add alpha_beta

--- skynet.py
+++ skynet.py
@@ -1,16 +1,14 @@
 
 
-def alphabeta_search(state, game, d=4, cutoff_test=None, eval_fn=None):
+def alphabeta_search(game, d=1, cutoff_test=None, eval_fn=None):
     """Search game to determine best action; use alpha-beta pruning.
     This version cuts off search and uses an evaluation function."""
-
-    player = game.to_move(state)
 
     def max_value(state, alpha, beta, depth):
         if cutoff_test(state, depth):
             return eval_fn(state)
         v = -float("inf")
-        for (a, s) in game.successors(state):
+        for (a, s) in game.successors():
             v = max(v, min_value(s, alpha, beta, depth+1))
             if v >= beta:
                 return v
@@ -21,61 +19,18 @@ def alphabeta_search(state, game, d=4, cutoff_test=None, eval_fn=None):
         if cutoff_test(state, depth):
             return eval_fn(state)
         v = float("inf")
-        for (a, s) in game.successors(state):
+        for (a, s) in game.successors():
             v = min(v, max_value(s, alpha, beta, depth+1))
             if v <= alpha:
                 return v
             beta = min(beta, v)
         return v
 
-    cutoff_test = (cutoff_test or
-                   (lambda state, depth: depth > d or game.terminal_test(state)))
-    eval_fn = eval_fn or (lambda state: game.utility(state, player))
-    action, state = argmax(game.successors(state),
-                           lambda a, s: min_value(s, -float("inf"), float("inf"), 0))
+    cutoff_test = lambda state, depth: depth > d or game.win_check()
+    eval_fn = game.heuristic
+    action, state = argmax(game.successors(),
+                           lambda move: min_value(move[1], -float("inf"), float("inf"), 0))
     return action
-
-
-class Game:
-    """A game is similar to a problem, but it has a utility for each
-    state and a terminal test instead of a path cost and a goal
-    test. To create a game, subclass this class and implement
-    legal_moves, make_move, utility, and terminal_test. You may
-    override display and successors or you can inherit their default
-    methods. You will also need to set the .initial attribute to the
-    initial state; this can be done in the constructor."""
-
-    def legal_moves(self, state):
-        "Return a list of the allowable moves at this point."
-        abstract
-
-    def make_move(self, move, state):
-        "Return the state that results from making a move from a state."
-        abstract
-
-    def utility(self, state, player):
-        "Return the value of this final state to player."
-        abstract
-
-    def terminal_test(self, state):
-        "Return True if this is a final state for the game."
-        return not self.legal_moves(state)
-
-    def to_move(self, state):
-        "Return the player whose move it is in this state."
-        return state.to_move
-
-    def display(self, state):
-        "Print or otherwise display the state."
-        print(state)
-
-    def successors(self, state):
-        "Return a list of legal (move, state) pairs."
-        return [(move, self.make_move(move, state))
-                for move in self.legal_moves(state)]
-
-    def __repr__(self):
-        return '<%s>' % self.__class__.__name__
 
 
 def argmin(seq, fn):
@@ -83,7 +38,8 @@ def argmin(seq, fn):
     >>> argmin(['one', 'to', 'three'], len)
     'to'
     """
-    best = seq[0]; best_score = fn(best)
+    best = seq[0]
+    best_score = fn(best)
     for x in seq:
         x_score = fn(x)
         if x_score < best_score:
@@ -97,7 +53,3 @@ def argmax(seq, fn):
     'three'
     """
     return argmin(seq, lambda x: -fn(x))
-
-
-if __name__ == '__main__':
-    alphabeta_search()
